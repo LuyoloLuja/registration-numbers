c9d648277e370a28757b87881a96c20c8a0e38ee
Luyolo <luyolom19@gmail.com>
2020-07-13 17:36:31 +0200

    first commit

---
+++ JS/registration_number.js
@@ -0,0 +1,5 @@
+var textElement = document.querySelector(".reg-textbox");
+var addButton = document.querySelector(".addButton");
+var plateNumberTwo = document.querySelector(".reg-one");
+var plateNumberTwo = document.querySelector(".reg-two");
+var plateNumberThree = document.querySelector(".reg-three");
