c97b6ee14fdd90153ab2e33362cd0e3cc510ad23
Luyolo <luyolom19@gmail.com>
2020-07-14 14:21:12 +0200

    added dropdown list

--- JS/registration_number.js
+++ JS/registration_number.js
@@ -1,5 +1,14 @@
 var textElement = document.querySelector(".reg-textbox");
 var addButton = document.querySelector(".addButton");
-var plateNumberTwo = document.querySelector(".reg-one");
+var plateNumberOne = document.querySelector(".reg-one");
 var plateNumberTwo = document.querySelector(".reg-two");
 var plateNumberThree = document.querySelector(".reg-three");
+
+function regNumbers(){
+
+  var regNumber = textElement.value;
+  if (regNumber) {
+    plateNumberOne.innerHTML === regNumber;
+  }
+}
+addButton.addEventListener('click', regNumbers);
