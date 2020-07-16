091d141cc223ba6ae4d51f50494d6cbe51d05283
Luyolo <luyolom19@gmail.com>
2020-07-16 15:27:24 +0200

    fixed list style

--- JS/registration_number.js
+++ JS/registration_number.js
@@ -1,16 +1,27 @@
 var textElement = document.querySelector(".reg-textbox");
 var addButton = document.querySelector(".addButton");
-var regList = document.querySelector(".regList");
+var regList = document.querySelector(".reg-plate");
 
 var regFactoryInstance = RegFactory();
 
 var regNumbers = [];
 function appendRegNumbers(plates){
   regList.innerHTML = "";
+
   for(var i = 0; i < plates.length; i++){
     let currentPlate = plates[i];
     let addingElement = document.createElement("li");
+    addingElement.innerHTML = currentPlate;
     regList.appendChild(addingElement);
   }
 }
-addButton.addEventListener('click', );
+
+function textValue(){
+  let theTextValue = "Registration Number";
+  if(textElement.value){
+    theTextValue = textElement.value;
+  }
+  regNumbers.push(theTextValue);
+  appendRegNumbers(regNumbers);
+}
+addButton.addEventListener('click', textValue);
