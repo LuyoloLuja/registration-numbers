aec48e862b0b006633ce0dc68c820e69cfec786f
Luyolo <luyolom19@gmail.com>
2020-07-23 15:24:43 +0200

    added filter factory function

--- JS/registration_number.js
+++ JS/registration_number.js
@@ -14,16 +14,13 @@ function textValue(){
 
   theTextValue = textElementValue.toUpperCase().trim();
   
-  if (theTextValue !== "" && regNumbers.length <= 14) {
+  if (theTextValue !== "" && regNumbers.length <= 10) {
     
     if (!regNumbers.includes(theTextValue)) {
-      if(theTextValue.startsWith("CA ") || theTextValue.startsWith("CL ") || theTextValue.startsWith("CJ ") || theTextValue.startsWith("CAG ")){
+      regFactoryInstance.regSelection(theTextValue)    
       regNumbers.push(theTextValue);
       appendRegNumbers(regNumbers);
     }
-    }  
-    /*regFactoryInstance.regSelection(theTextValue)
-      appendRegNumbers(regNumbers);*/
   }
 }
 
@@ -56,3 +53,62 @@ function listFiltering(){
 
 }
 filterButton.addEventListener('click', listFiltering);
+
+
+
+/*var textElement = document.querySelector(".reg-textbox");
+var addButton = document.querySelector(".addButton");
+var regList = document.querySelector(".reg-plate");
+var registration = document.querySelector(".drop-down");
+var filterButton = document.querySelector(".filterButton");
+
+var regFactoryInstance = RegFactory();
+
+var regNumbers = [];
+
+function textValue(){
+  let theTextValue = "";
+  let textElementValue = textElement.value;
+
+  theTextValue = textElementValue.toUpperCase().trim();
+  
+  if (theTextValue !== "" && regNumbers.length <= 14) {
+    
+    if (!regNumbers.includes(theTextValue)) {
+      if(theTextValue.startsWith("CA ") || theTextValue.startsWith("CL ") || theTextValue.startsWith("CJ ") || theTextValue.startsWith("CAG ")){
+      regNumbers.push(theTextValue);
+      appendRegNumbers(regNumbers);
+    }
+  }
+}
+
+addButton.addEventListener('click', textValue);
+
+function appendRegNumbers(plates){
+  regList.innerHTML = "";
+
+  for(var i = 0; i < plates.length; i++){
+    let currentPlate = plates[i];
+    let addingElement = document.createElement("li");
+
+    addingElement.innerHTML = currentPlate;
+    regList.appendChild(addingElement);
+  }
+
+}
+
+function listFiltering(){
+  var selectedCity = registration.options[registration.selectedIndex].value;
+  var filteredReg = [];
+  
+  for(var i = 0; i < regNumbers.length; i++){
+    var currentReg = regNumbers[i];
+    if (currentReg.startsWith(selectedCity)) {
+      filteredReg.push(currentReg);
+    }
+  }
+  appendRegNumbers(filteredReg);
+
+}
+filterButton.addEventListener('click', listFiltering);
+*/
--- factory-function/registration-factory.js
+++ factory-function/registration-factory.js
@@ -2,31 +2,29 @@ function RegFactory(){
   var registrationNumbers = [];
 
   function regSelection(selectReg){
-	    if (!registrationNumbers.includes(selectReg)) {
-	    	if(selectReg.startsWith("CA") || selectReg.startsWith("CJ") || selectReg.startsWith("CAG") || selectReg.startsWith("CL") && selectReg !== undefined){
-	    	registrationNumbers.push(selectReg);
-	    	// TO DO : should append the list function
-	    }
+  		if(selectReg.startsWith("CA ") || selectReg.startsWith("CL ") || selectReg.startsWith("CJ ") || selectReg.startsWith("CAG ")){
+      	return selectReg;
     }
   }
 
-  function appendRegNums(registrations){
+  /*function appendRegNums(registrations){
 
   	for(var i = 0; i < registrations.length; i++){
-  		domElement = "";	// should do this on my dom
-  		
+
   		var currentReg = registrations[i];
-  		var addElement = document.createElement("li");
+  		let addingElement = document.createElement("li");
 
-  		addElement.innerHTML = currentReg; // should probably do this on my DOM
-  		domElement.appendChild(addElement); //should also do this on my dom
-
+  		addingElement.innerHTML = currentReg;
   	}
+  }*/
+
+  function filter(){
+  	var regFilter = []
   }
 
 
   return {
     regSelection,
-    appendRegNums
+    //appendRegNums
   }
 }
